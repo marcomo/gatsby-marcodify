42141c379e7742f9486cddeac0ebcb3e0aaf7d73
Marco Morales <marco.bbcm@gmail.com>
2021-11-08 14:41:17 -0800

    Adds assets for channelmaker project
    Adds images and markdown for vadio channelmaker project.
    Removes white background from images.

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,7 @@ module.exports = {
             resolve: "gatsby-remark-images",
             options: {
               maxWidth: 800,
+              backgroundColor: "transparent",
               withWebp: true,
               tracedSVG: {
                 color: "coral",
