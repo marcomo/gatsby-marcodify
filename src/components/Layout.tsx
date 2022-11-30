8c7eeedb75b13a95a3c416931ea47fc89debfc2b
Marco Morales <marco.bbcm@gmail.com>
2022-11-30 12:45:01 -0800

    Adds MainNav component and logo animation
    - Move nav to MainNav component
    - Adds animation styles for logo hover

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,16 +1,12 @@
 import React from "react";
+import MainNav from "./MainNav";
 import SideNav from "./SideNav";
 
 const Layout = (props) => {
   return (
     <div id="mdfy-wrapper">
       <header>
-        <nav>
-          <div id="mdfy-brand-logo">
-            <span>mar</span>
-            <span>codify</span>
-          </div>
-        </nav>
+        <MainNav />
       </header>
       <main>
         <aside>
---
+++ src/components/MainNav.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainNav: React.FunctionComponent = () => {
+  return (
+    <nav>
+      <div className="mdfy-brand-logo" />
+    </nav>
+  );
+};
+
+export default MainNav;
