49da29adf9b60898f7745fc1b1bc1301e68a132d
Marco Morales <me@marcodify.com>
2023-01-27 22:17:24 +0000

    Updates trendsnow content
    - Slims down copy
    - Moves 'concept' section to a component
    - moves project images logic to parent of ProjectSection
    - adds more queries to React-Media for desktop, mobile,
      and tablet
    - Gives section queries individual names
    - Updates territories mobile images
    - Adds trend card progression images
    - Adds early concept image
    - adds MVP search image
    - adds songs on the move desktop image
    - adds MVP territories image
    - Updates global header grids
    - Reorders cascading of grid classes
    - Converts all 1x1 grid classes to flex column

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -7,71 +7,96 @@ import Grid from './layouts/Grid';
 const ProjectSection: React.FunctionComponent<
   PropsWithChildren<{
     id: string;
-    data: Queries.MdxQuery;
+    images?: {
+      image: Queries.SearchMdxQuery['mdx']['frontmatter']['images'][number];
+      alt: Queries.SearchMdxQuery['mdx']['frontmatter']['imageAlts'][number];
+    }[];
+    contentGridArea?: number;
+    imageGridArea?: number;
     imgGridClassName?: string;
     imgGridItemClassNames?: Record<`gridarea-${number}`, string>;
+    sectionGridClassNames?: Record<`gridarea-${number}`, string>;
+    caption?: string;
   }>
 > = (props) => {
-  const imageIndexes = props.data?.mdx.frontmatter.images.reduce(
-    (acc, img, i) => {
-      if (img.childrenImageSharp[0].original.src.includes(props.id)) {
-        acc.push(i);
-      }
-      return acc;
-    },
-    []
-  );
-
-  const frontmatter = {
-    ...props.data.mdx.frontmatter,
-    images: props.data.mdx.frontmatter.images.filter((img, i) =>
-      imageIndexes.includes(i)
-    ),
-  };
-
   return (
-    <Media query={'(max-width: 480px)'}>
+    <Media
+      queries={{
+        sm: '(max-width: 480px)',
+        md: '(max-width: 1200px)',
+        lg: '(min-width: 1201px)',
+      }}
+    >
       {(matches) => (
         <Grid
           lgrows={1}
           lgcolumns={2}
           mdcolumns={1}
           mdrows={2}
-          className="thin-row-gaps"
+          className={`thin-row-gaps ${
+            matches.lg ? 'mb-8' : matches.sm ? 'mb-2' : 'mb-4'
+          }`}
         >
-          <div className="gridarea-1">{props.children}</div>
-          <div className="gridarea-2">
-            <div className={props.imgGridClassName}>
-              {frontmatter.images.map((img, i) => {
-                const alt = frontmatter.imageAlts[i];
-                return (
-                  <div
-                    key={`${props.id}-${i}`}
-                    className={`gridarea-${i + 1} ${
-                      props.imgGridItemClassNames?.[`gridarea-${i + 1}`] ?? ''
-                    }`}
-                  >
-                    <Image alt={alt} allowModal={!matches} showCaption={false}>
-                      <GatsbyImage
-                        key={`image-${i}`}
-                        image={getImage(
-                          img.childrenImageSharp[0].gatsbyImageData
-                        )}
-                        alt={alt}
-                        objectFit="contain"
-                        imgStyle={{ maxHeight: '75vh' }}
-                        style={{ maxHeight: '75vh' }}
-                      />
-                    </Image>
-                  </div>
-                );
-              })}
-            </div>
+          <div
+            className={`gridarea-${props.imageGridArea === 1 ? 2 : 1} ${
+              props.sectionGridClassNames?.[`gridarea-1`] ?? ''
+            }`}
+          >
+            {props.children}
           </div>
+          {props.images ? (
+            <div
+              className={`gridarea-${props.imageGridArea} ${
+                props.sectionGridClassNames?.[
+                  `gridarea-$${props.imageGridArea}`
+                ] ?? ''
+              }`}
+            >
+              <div className={props.imgGridClassName}>
+                {props.images.map((img, i) => {
+                  const alt = img.alt;
+                  return (
+                    <div
+                      key={`${props.id}-${i}`}
+                      className={`gridarea-${i + 1} ${
+                        props.imgGridItemClassNames?.[`gridarea-${i + 1}`] ?? ''
+                      }`}
+                    >
+                      <Image
+                        alt={alt}
+                        allowModal={!matches}
+                        showCaption={false}
+                      >
+                        <GatsbyImage
+                          key={`image-${i}`}
+                          image={getImage(
+                            img.image.childrenImageSharp[0].gatsbyImageData
+                          )}
+                          alt={alt}
+                          objectFit="contain"
+                          imgStyle={{ maxHeight: '75vh' }}
+                          style={{ maxHeight: '75vh' }}
+                        />
+                      </Image>
+                    </div>
+                  );
+                })}
+              </div>
+              {props.caption ? (
+                <figure className="pt-half">
+                  <figcaption>{props.caption}</figcaption>
+                </figure>
+              ) : null}
+            </div>
+          ) : null}
         </Grid>
       )}
     </Media>
   );
+};
+
+ProjectSection.defaultProps = {
+  imageGridArea: 2,
 };
 
 export default ProjectSection;
--- src/components/projects/ingrooves/OnTheMove.tsx
+++ src/components/projects/ingrooves/OnTheMove.tsx
@@ -2,20 +2,23 @@ import { useStaticQuery } from 'gatsby';
 import { graphql } from 'gatsby';
 import React, { PropsWithChildren } from 'react';
 import ProjectSection from '../../ProjectSection';
+import Image from '../../Image';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+import Media from 'react-media';
 
 const OnTheMove: React.FunctionComponent<
   PropsWithChildren<{
     id: string;
   }>
 > = (props) => {
-  const data = useStaticQuery<Queries.MdxQuery>(graphql`
-    query Mdx {
+  const data = useStaticQuery<Queries.OnTheMoveMdxQuery>(graphql`
+    query OnTheMoveMdx {
       mdx(internal: { contentFilePath: { regex: "/(/trendsnow)/" } }) {
         id
         frontmatter {
           images {
             childrenImageSharp {
-              gatsbyImageData(layout: CONSTRAINED, width: 650)
+              gatsbyImageData(layout: CONSTRAINED, width: 1200)
               original {
                 src
               }
@@ -28,14 +31,62 @@ const OnTheMove: React.FunctionComponent<
     }
   `);
 
+  const images = data.mdx.frontmatter.images
+    .reduce((acc, img, i) => {
+      if (img.childrenImageSharp[0].original.src.includes(props.id)) {
+        acc.push(i);
+      }
+      return acc;
+    }, [])
+    .map((index) => ({
+      image: data.mdx.frontmatter.images[index],
+      alt: data.mdx.frontmatter.imageAlts[index],
+    }));
+
+  const desktop = images.find((img) => img.alt.includes('desktop'));
+  const mobile = images.find((img) => img.alt.includes('mobile'));
+
   return (
-    <ProjectSection
-      id={props.id}
-      data={data}
-      imgGridClassName={[`lg-grid-1-1`, `md-grid-1-1`, `sm-grid-1-1`].join(' ')}
+    <Media
+      queries={{
+        sm: '(max-width: 480px)',
+        md: '(max-width: 1200px)',
+        lg: '(min-width: 1201px)',
+      }}
     >
-      {props.children}
-    </ProjectSection>
+      {(matches) => (
+        <ProjectSection
+          id={props.id}
+          images={[mobile]}
+          imgGridClassName={[`lg-grid-1-1`, `md-grid-1-1`, `sm-grid-1-1`].join(
+            ' '
+          )}
+          sectionGridClassNames={{
+            'gridarea-1': matches.lg ? 'pt-2' : '',
+          }}
+          caption={mobile.alt}
+        >
+          <h3>On the Move</h3>
+          <Image
+            alt={desktop.alt}
+            allowModal={!matches}
+            showCaption={true}
+            className={matches.sm ? 'no-figure-margins' : ''}
+          >
+            <GatsbyImage
+              image={getImage(
+                desktop.image.childrenImageSharp[0].gatsbyImageData
+              )}
+              alt={desktop.alt}
+              objectFit="contain"
+              imgStyle={{ maxHeight: '75vh' }}
+              style={{ maxHeight: '75vh' }}
+            />
+          </Image>
+          {props.children}
+        </ProjectSection>
+      )}
+    </Media>
   );
 };
 
