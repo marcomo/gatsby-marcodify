f222f17bc74a7399846bad67ace159a45c0d13fe
Marco Morales <me@marcodify.com>
2023-02-16 15:23:35 +0000

    Adds 'about' section to home page
    - And contact email to home page and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,11 +5,20 @@ import * as styles from './footer.module.scss';
 const Footer: React.FunctionComponent = () => {
   return (
     <footer className={styles.footer}>
-      <div className="flex-row flex-item space-between align-center width-control">
+      <div className={styles.inner + ' flex-item space-between width-control'}>
         ©2023 Marco Morales
-        <ExternalLink href="https://www.linkedin.com/in/marcodify">
-          <i className={styles.linkedin + ' fa-brands fa-linkedin'}></i>
-        </ExternalLink>
+        <div className={styles.contact}>
+          <div className="flex-row align-center">
+            <i className={styles.icon + ' fa-solid fa-envelope mr-1'}></i>
+            <span>me @ marcodify dot com</span>
+          </div>
+          <ExternalLink
+            href="https://www.linkedin.com/in/marcodify"
+            className="no-underline"
+          >
+            <i className={styles.icon + ' fa-brands fa-linkedin'}></i>
+          </ExternalLink>
+        </div>
       </div>
     </footer>
   );
