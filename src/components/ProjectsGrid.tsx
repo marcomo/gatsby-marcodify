deafb4b19a57cc155e48f95bd342a304a5af0494
Marco Morales <marco.bbcm@gmail.com>
2023-09-15 16:32:18 -0700

    Adds Grid component to Projects grid item copy

--- src/components/ProjectsGrid.tsx
+++ src/components/ProjectsGrid.tsx
@@ -1,9 +1,10 @@
-import React, { FC } from 'react';
-import ConditionalRender from './ConditionalRender';
-import { Link, graphql, useStaticQuery } from 'gatsby';
-import { GatsbyImage } from 'gatsby-plugin-image';
-import NonBreakingText from './NonBreakingText';
-import * as styles from '../stylesheets/projects.module.scss';
+import React, { FC } from 'react'
+import ConditionalRender from './ConditionalRender'
+import { Link, graphql, useStaticQuery } from 'gatsby'
+import { GatsbyImage } from 'gatsby-plugin-image'
+import NonBreakingText from './NonBreakingText'
+import * as styles from '../stylesheets/projects.module.scss'
+import Grid from './layouts/Grid'
 
 const ProjectsGrid: FC = () => {
   // Get all projects so we can generate a list in the Projects view
@@ -41,7 +42,7 @@ const ProjectsGrid: FC = () => {
       }
     }
   `)
-  const { projects } = data.allMdx;
+  const { projects } = data.allMdx
   return (
     <div className={styles.projects}>
       {projects.map((proj) => {
@@ -50,30 +51,42 @@ const ProjectsGrid: FC = () => {
             shouldRender={!!proj.node.frontmatter}
             key={proj.node.id}
           >
-            <Link className="thumb-link" to={'/projects/' + proj.node.frontmatter?.slug ?? ''}>
-              <div className="flex-row justify-center">
-                {proj.node.frontmatter?.thumb?.childImageSharp
-                  ?.fluid ? (
+            <Link
+              className='thumb-link'
+              to={'/projects/' + proj.node.frontmatter?.slug ?? ''}
+            >
+              <div className='flex-row justify-center'>
+                {proj.node.frontmatter?.thumb?.childImageSharp?.fluid ? (
                   <GatsbyImage
-                    image={
-                      proj.node.frontmatter?.thumb?.childImageSharp
-                        ?.fluid
-                    }
-                    alt=""
-                    loading="eager"
+                    image={proj.node.frontmatter?.thumb?.childImageSharp?.fluid}
+                    alt=''
+                    loading='eager'
                   />
                 ) : null}
               </div>
-              <h5 className="h5">{proj.node.frontmatter?.company}</h5>
-              <h4 className="h4 m-0"><NonBreakingText text={proj.node.frontmatter?.heading} /></h4>
-              <p>{proj.node.frontmatter?.description}</p>
+              <Grid
+                rows={2}
+                columns={1}
+                templateRows='1fr auto'
+                className='mt-4'
+              >
+                <div className='gridarea-1 mt-1'>
+                  <h5 className='h5'>{proj.node.frontmatter?.company}</h5>
+                  <h4 className='h4 gridarea-2 m-0'>
+                    <NonBreakingText text={proj.node.frontmatter?.heading} />
+                  </h4>
+                </div>
+                <p className='gridarea-2'>
+                  {proj.node.frontmatter?.description}
+                </p>
+              </Grid>
+              <div className='grid-1-3'></div>
             </Link>
           </ConditionalRender>
-        );
+        )
       })}
     </div>
-  );
-};
+  )
+}
 
-
-export default ProjectsGrid;
+export default ProjectsGrid
